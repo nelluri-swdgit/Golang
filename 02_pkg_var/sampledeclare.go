f0c453032ff9bfaddb4579892d1061dc64bc28cb
nelluri-swdgit <60553349+nelluri-swdgit@users.noreply.github.com>
2022-07-11 22:33:49 +0200

    Type conversion sample code

--- 02_pkg_var/sampledeclare.go
+++ 02_pkg_var/sampledeclare.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	// short declaration oprator (sdo)
+	// short declaration operator (sdo)
 	// Declare a variale and assign a value to it
 	// useful to declare a variable inside a function
 	x := 10
@@ -26,5 +26,35 @@ func main() {
 
 	// Primitive data types aka basic/built-in ex: bool, int, string
 	// composite data types, which hold the data of other types ex: arrays, slice, structs
+
+	TypeCoversion()
+
+}
+
+func TypeCoversion(){
+	// declare a variable with a built-in type
+	var a int
+
+	// create your own type and declare a  variable with that
+	type chilli int
+	var b chilli 
+
+	a = 42
+	b = 44
+
+	// prints the values and their existing types
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+	// try dynamically changing the variable types and it will not work
+	//  i.e. a = b will not work because both of them are declared as different types
+	// and GoLang is a static programming language
+
+	// now we try the type conversion, which is called as type casting in other programming languages
+	a = int(b)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
 
 }
