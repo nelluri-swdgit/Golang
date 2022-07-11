b4dc888238d8ad5c6699bfb084a50e64cb82749a
nelluri-swdgit <60553349+nelluri-swdgit@users.noreply.github.com>
2022-07-11 16:57:07 +0200

    updated info on variable declaration and data types check

--- 02_pkg_var/sampledeclare.go
+++ 02_pkg_var/sampledeclare.go
@@ -3,10 +3,28 @@ package main
 import "fmt"
 
 func main() {
+
+	// short declaration oprator (sdo)
+	// Declare a variale and assign a value to it
+	// useful to declare a variable inside a function
 	x := 10
 	fmt.Println(x)
 	y := 20
 	fmt.Println(y)
 	z := "Bond Bond..." + "James Bond"
 	fmt.Println(z)
+
+	// excluding sdo with the keyword var
+	// use this when you need to declare a variable outside the function
+	// suggestion: Limit the scope of the variable to the requird functions only
+	var n = "Mercedes"
+	fmt.Println(n)
+
+	// Finding out the type of a variable
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
+
+	// Primitive data types aka basic/built-in ex: bool, int, string
+	// composite data types, which hold the data of other types ex: arrays, slice, structs
+
 }
